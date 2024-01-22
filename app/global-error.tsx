2ee402e10af8cb77d5730b6bfb7117e25459ad4e
Imanuel Pundoko <ilomon10@gmail.com>
2024-01-22 10:44:16 +0800

    Fix global error

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -4,7 +4,13 @@ import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
 import { useEffect } from "react";
 
-export default function GlobalError({ error }: { error: Error }) {
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
   useEffect(() => {
     Sentry.captureException(error);
   }, [error]);
@@ -12,7 +18,8 @@ export default function GlobalError({ error }: { error: Error }) {
   return (
     <html>
       <body>
-        <Error statusCode={404} />
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
       </body>
     </html>
   );
