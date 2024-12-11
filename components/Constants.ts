c164446c81b90dbef33500af75a49c7a1f9b257b
Imanuel Pundoko <ilomon10@gmail.com>
2024-12-11 14:23:07 +0800

    build: improve build phase by removing initial build generate static params chore: environtment for google analytics id and umami url

--- components/Constants.ts
+++ components/Constants.ts
@@ -4,6 +4,9 @@ export const SERVER_URL = process.env.serverUrl as string;
 export const SERVER_API_KEY = process.env.serverApiKey as string;
 export const CURRENT_HOSTNAME = process.env.currentHostName as string;
 export const GOOGLE_API_KEY = process.env.googleApiKey as string;
+export const GOOGLE_ANALYTICS_ID = process.env.googleAnalyticsId as string;
+export const UMAMI_ID = process.env.umamiId as string;
+export const UMAMI_URL = process.env.umamiUrl as string;
 export const IMAGEKIT_URL = process.env.imagekitUrl as string;
 export const IMAGEKIT_PUBLIC_KEY = process.env.imagekitPublicKey as string;
 export const IMAGEKIT_PRIVATE_KEY = process.env.imagekitPrivateKey as string;
@@ -15,6 +18,9 @@ export const CONSTANTS = {
   SERVER_API_KEY,
   CURRENT_HOSTNAME,
   GOOGLE_API_KEY,
+  GOOGLE_ANALYTICS_ID,
+  UMAMI_ID,
+  UMAMI_URL,
   IMAGEKIT_URL,
   IMAGEKIT_PUBLIC_KEY,
   IMAGEKIT_PRIVATE_KEY,
--- components/component/Analytics.tsx
+++ components/component/Analytics.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { CONSTANTS } from "../Constants";
 
-export const Analytics: React.FC<React.PropsWithChildren> = ({ children }) => {
+export const Analytics: React.FC = () => {
   return (
     <>
       <script
         defer
-        src="https://analytics-umami-686f9c-192-168-1-13.185131101.xyz/script.js"
-        data-website-id="a187c3a5-c4f4-4282-833a-4ecf6d49908a"
+        src={`https://${CONSTANTS.UMAMI_URL}/script.js`}
+        data-website-id={CONSTANTS.UMAMI_ID}
       ></script>
-      <GoogleAnalytics gaId="G-Q29Q1QRVTP" />
+      <GoogleAnalytics gaId={CONSTANTS.GOOGLE_ANALYTICS_ID} />
     </>
   );
 };
